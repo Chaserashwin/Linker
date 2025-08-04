ac73a8069d91472091d3edae6284d04fd1bdb698
Ashwin Jaiswal <jaiswalashwin398@gmail.com>
2025-08-04 15:25:21 +0530

    project partially completed

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,12 +1,22 @@
 import { BadgeCheck } from "lucide-react";
 import moment from "moment";
-import React from "react";
+import React, { useState } from "react";
+import { dummyUserData } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const PostCard = ({ post }) => {
   const postWithHashtags = post.content.replace(
     /(#\w+)/g,
     '<span class="text-indigo-600">$1</span>'
   );
+  const [likes, setLikes] = useState(post.likes_count);
+  const currentUser = useSelector((state) => state.user.value);
+
+  // const handleLike = async () => {};
+
+  const navigate = useNavigate();
+
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl">
       {/* User Info */}
